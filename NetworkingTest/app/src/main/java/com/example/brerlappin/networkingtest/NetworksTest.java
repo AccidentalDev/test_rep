c49e6752ad56cafeda59b6de5bdc9c838e3fb9d6
AccidentalDev <serge_cfcu@hotmail.com>
2016-12-15 13:30:21 -0600

    Uploaded project files

---
+++ NetworkingTest/app/src/main/java/com/example/brerlappin/networkingtest/NetworksTest.java
@@ -0,0 +1,13 @@
+package com.example.brerlappin.networkingtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NetworksTest extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_networks_test);
+    }
+}
